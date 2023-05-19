edb53aa9437cae45e91380f6f4c43a77d4a40e4b
arsumner <122269006+arsumner@users.noreply.github.com>
2023-05-19 13:53:06 -0700

    Update main.js to incorporate images

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-function rollDice() {
-    var numDice = document.getElementById("numDice").value;
-    var resultsDiv = document.getElementById("results");
-    resultsDiv.innerHTML = "";
+// JavaScript code for rolling the die
 
-    for (var i = 0; i < numDice; i++) {
-        var rollResult = Math.floor(Math.random() * 6) + 1;
-        var resultText = document.createTextNode("You rolled: " + rollResult);
-        resultsDiv.appendChild(resultText);
-        resultsDiv.appendChild(document.createElement("br"));
-    }
+function rollDie() {
+    var die = document.getElementById('die');
+    var result = Math.floor(Math.random() * 6) + 1; // Randomize the result from 1 to 6
+
+    die.style.animation = 'none'; // Reset animation
+    void die.offsetWidth; // Trigger reflow to restart the animation
+
+    die.style.backgroundImage = 'url(' + images + '.png)'; // Images is file name pattern for die images
 }
+
+// Call the rollDie function to display the initial die
+rollDie();
