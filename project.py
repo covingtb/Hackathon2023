8c614d4fdaea735d6d37d6b65d4d011cea12d52a
arsumner <122269006+arsumner@users.noreply.github.com>
2023-05-19 10:36:58 -0700

    roll dice base code

--- project.py
+++ project.py
@@ -1,5 +1,18 @@
-# Authors: Brenden Covington,
+# Authors: Brenden Covington, Mandy Sumner, Sonja Lavin
 # Githubs: covingtb,
 # Date started: 5/19/2023
 # Description:
+
+import random
+
+def roll_dice(num_dice):
+    for _ in range(num_dice):
+        roll_result = random.randint(1, 6)
+        print("You rolled:", roll_result)
+
+while True:
+    num_dice = int(input("Enter the number of dice to roll (0 to exit): "))
+    if num_dice == 0:
+        break
+    roll_dice(num_dice)
 
