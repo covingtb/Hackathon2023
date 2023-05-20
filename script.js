479c32cd0144d7a1a42f57c308d20905b685719e
lavinso <122298877+lavinso@users.noreply.github.com>
2023-05-19 21:45:53 -0700

    I'm hoping this works!
    I used chatgpt to code it all in pycharm. It works on my end, I hope it works for you guys. I would like to name the buttons better, maybe add a scorecard to keep track of say up to ten rounds. Have something more exciting happen if you win. Hopefully we can think of another way to add ai in too. but I'm tired :)

--- script.js
+++ script.js
@@ -1,14 +1,84 @@
-// JavaScript code for rolling the die
+const diceImages = [
+  "dice1.png",
+  "dice2.png",
+  "dice3.png",
+  "dice4.png",
+  "dice5.png",
+  "dice6.png"
+];
 
-function rollDie() {
-    var die = document.getElementById('die');
-    var result = Math.floor(Math.random() * 6) + 1; // Randomize the result from 1 to 6
+let playerScore = 0;
+let botScore = 0;
 
-    die.style.animation = 'none'; // Reset animation
-    void die.offsetWidth; // Trigger reflow to restart the animation
+// Get the necessary elements from the DOM
+const playerDice = document.getElementById('player-dice');
+const botDice = document.getElementById('bot-dice');
+const rollPlayerButton = document.getElementById('roll-player-button');
+const rollBotButton = document.getElementById('roll-bot-button');
 
-    die.style.backgroundImage = 'url(' + images + '.png)'; // Replace 'result' with the file name pattern for your die images
+// Function to generate a random number between 1 and 6
+function getRandomNumber() {
+  return Math.floor(Math.random() * 6) + 1;
 }
 
-// Call the rollDie function to display the initial die
-rollDie();
+// Function to roll the player dice and update the score
+function rollPlayerDice() {
+  // Generate a random number between 1 and 6
+  const randomNumber = getRandomNumber();
+
+  // Update the player dice image source
+  playerDice.src = `dice${randomNumber}.png`;
+
+  // Update the player score
+  playerScore += randomNumber;
+  document.getElementById('player-score').textContent = `Player Score: ${playerScore}`;
+
+  checkWinner();
+}
+
+// Function to roll the bot dice and update the score
+function rollBotDice() {
+  // Generate a random number between 1 and 6
+  const randomNumber = getRandomNumber();
+
+  // Update the bot dice image source
+  botDice.src = `dice${randomNumber}.png`;
+
+  // Update the bot score
+  botScore += randomNumber;
+  document.getElementById('bot-score').textContent = `Bot Score: ${botScore}`;
+
+  checkWinner();
+}
+
+// Function to check the winner and handle game over condition
+function checkWinner() {
+  if (playerScore >= 20 || botScore >= 20) {
+    let winner;
+    if (playerScore > botScore) {
+      winner = 'Player';
+    } else if (botScore > playerScore) {
+      winner = 'Bot';
+    } else {
+      winner = 'It\'s a tie';
+    }
+    alert(`Game over! ${winner} wins!`);
+    resetScores();
+  }
+}
+
+// Function to reset scores and dice images
+function resetScores() {
+  playerScore = 0;
+  botScore = 0;
+  document.getElementById('player-score').textContent = 'Player Score: 0';
+  document.getElementById('bot-score').textContent = 'Bot Score: 0';
+  playerDice.src = 'dice-empty.png';
+  botDice.src = 'dice-empty.png';
+}
+
+// Add click event listeners to the buttons
+rollPlayerButton.addEventListener('click', rollPlayerDice);
+rollBotButton.addEventListener('click', rollBotDice);
+
+
