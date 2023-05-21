42b3fcd43e9fc52dc247c05b207d5d9f57c93d50
lavinso <lavinso@oregonstate.edu>
2023-05-21 09:05:12 -0700

    With friendly bot

--- script.js
+++ script.js
@@ -13,8 +13,20 @@ let playerWins = 0;
 let botWins = 0;
 
 let botRollTimer = null;
+// Flag to track whether the bot is rolling
 let isBotRolling = false;
 
+function displayBotSpeech() {
+  const botSpeechBubble = document.getElementById('bot-speech-container');
+  botSpeechBubble.textContent = "Hi! Roll dice the dice to get a score. I will roll my dice after you. Whoever gets to 20 faster, or gets more points, wins!";
+  botSpeechBubble.style.display = "block";
+}
+
+function displayBotPlayAgain() {
+  const botSpeechBubble = document.getElementById('bot-speech-container');
+  botSpeechBubble.textContent = "Roll the dice and lets play again!";
+  botSpeechBubble.style.display = "block";
+}
 function rollDice(player) {
   const diceValue = Math.floor(Math.random() * 6) + 1;
   const diceImage = diceImages[diceValue - 1];
@@ -33,16 +45,18 @@ function rollDice(player) {
     isBotRolling = true;
     rollPlayerButton.disabled = true;
 
-    clearTimeout(botRollTimer);
+    clearTimeout(botRollTimer); // Clear any existing bot roll timer
     botRollTimer = setTimeout(() => {
       rollDice('bot');
       isBotRolling = false;
       rollPlayerButton.disabled = false;
-      checkWinner();
-    }, 1000);
-  } else {
-    checkWinner();
+      checkWinner(); // Move the checkWinner() call here
+    }, 1000); // Set bot roll delay to .5 seconds (500 milliseconds)
   }
+
+  // Hide bot speech bubble after player rolls dice
+  const botSpeechBubble = document.getElementById('bot-speech-container');
+  botSpeechBubble.style.display = "none";
 }
 
 function checkWinner() {
@@ -59,17 +73,10 @@ function checkWinner() {
     } else {
       winner = 'Everyone';
     }
-
-    const dialog = document.getElementById('winner-dialog');
-    const dialogText = dialog.querySelector('.dialog-text');
-    dialogText.textContent = `Game over! ${winner} wins!`;
-
-    dialog.showModal();
+    alert(`Game over! ${winner} wins!`);
     showConfetti();
-    setTimeout(() => {
-      dialog.close();
-      resetScores();
-    }, 5000);
+    setTimeout(resetScores, 3000); // Delay in milliseconds (3 seconds in this example)
+    displayBotPlayAgain()
   }
 }
 
@@ -85,13 +92,13 @@ function resetScores() {
 
 function showConfetti() {
   const confettiContainer = document.getElementById('confetti-container');
-  const confettiCount = 50;
+  const confettiCount = 100; // Number of confetti pieces to generate
 
   for (let i = 0; i < confettiCount; i++) {
     const confetti = document.createElement('div');
     confetti.classList.add('confetti');
     confetti.style.left = `${Math.random() * 100}%`;
-    confetti.style.animationDelay = `${Math.random() * 3}s`;
+    confetti.style.animationDelay = `${Math.random() * 3}s`; // Randomize the delay for a staggered effect
     confettiContainer.appendChild(confetti);
   }
 }
@@ -99,7 +106,6 @@ function showConfetti() {
 function removeConfetti() {
   const confettiContainer = document.getElementById('confetti-container');
   while (confettiContainer.firstChild) {
-    confettiContainer.removeChild(confettiContainer.firstChild)
     confettiContainer.removeChild(confettiContainer.firstChild);
   }
 }
@@ -115,10 +121,7 @@ rollPlayerButton.addEventListener('click', () => {
   }
 });
 
-// Add a click event listener to the "Close" button in the winner dialog
-const dialogCloseButton = document.getElementById('dialog-close-button');
-dialogCloseButton.addEventListener('click', () => {
-  const dialog = document.getElementById('winner-dialog');
-  dialog.close();
-  resetScores();
+// Display bot speech bubble when the page loads
+window.addEventListener('load', () => {
+  displayBotSpeech();
 });
